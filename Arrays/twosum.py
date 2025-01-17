d833f0b75dd2cc1a2f655047e15adf4e196b9f97
Niyanta <niyantapandey@gmail.com>
2025-01-16 19:16:40 -0800

    cover all the edge cases in twosum

---
+++ Arrays/twosum.py
@@ -0,0 +1,17 @@
+from typing import List
+class Solution:
+    def twoSum(self, alist:List[int], target:int) -> List[int]:
+        result = []
+        adict = {}
+        for i, num in enumerate(alist):
+            complement = target-num
+            if complement in adict:
+                result.append([adict[complement], i])
+            adict[num] = i
+        return result
+                
+
+    
+solution = Solution()
+result = solution.twoSum([1,3,53,3,2,4],6)
+print(result)        
