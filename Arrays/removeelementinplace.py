18830f3a907304fb97f398410f5c97ca454c8a95
Niyanta <niyantapandey@gmail.com>
2025-01-17 11:23:10 -0800

    remove the element in place in the array

---
+++ Arrays/removeelementinplace.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeElement(self, alist, num):
+        blist = []
+        for i in range(0, len(alist)):
+            if alist[i] != num:
+                blist.append(alist[i])
+                
+        #return the length of the remaining elements after deleting all the occurrences of val
+        return len(blist)
+    
+    
+solution= Solution()
+result = solution.removeElement([1,34,22,12,12,4, 4, 4,4,4], 4)
+print(result)
